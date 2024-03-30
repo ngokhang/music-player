ddd2732a5efdeeafd9253e4012d4dfc66ce7cf83
ngokhang <ngokhang2002vn@gmail.com>
2024-03-31 01:32:57 +0700

    Chức năng next, prev bài hát, loop

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,49 +1,135 @@
-import AudioClass from "./Audio.js";
+const songsList = [
+  {
+    name: "Hit me up",
+    singer: "Binz",
+    path: "./assets/music/hitmeup.mp3",
+  },
+  {
+    name: "Nevada",
+    singer: "Vicetone",
+    path: "./assets/music/nevada.mp3",
+  },
+  {
+    name: "Simp gái 808",
+    singer: "Low G",
+    path: "./assets/music/simp808.mp3",
+  },
+];
 
 export default class MusicPlayer {
-  songsList = [
-    {
-      name: "Hit me up",
-      singer: "Binz",
-      path: "./assets/music/hitmeup.mp3",
-    },
-    {
-      name: "Nevada",
-      singer: "Vicetone",
-      path: "./assets/music/nevada.mp3",
-    },
-    {
-      name: "Simp gái 808",
-      singer: "Low G",
-      path: "./assets/music/simp808.mp3",
-    },
-  ];
   constructor() {
     this.$ = document.querySelector.bind(document);
-    this.currentSong = this.songsList[0];
-    this.audio = new AudioClass(this.currentSong);
+    this.currentSong = songsList[0];
+    this.audio = this.$("#audio");
+    this.audio.src = this.currentSong.path;
+    this.playBtn = this.$("#play-btn");
+    this.progress = this.$("#progress");
+    this.playBtn = this.$("#play-btn");
+    this.replayBtn = this.$("#replay-btn");
+    this.nextBtn = this.$("#next-btn");
+    this.prevBtn = this.$("#prev-btn");
+    this.isLoop = false;
+    this.isPlaying = false;
+    this.currentSongIndex =
+      songsList.findIndex((song) => song.name === this.currentSong.name) > 0
+        ? songsList.findIndex((song) => song.name === this.currentSong.name)
+        : 0;
 
     this.render();
+    this.play();
+    this.progressClick();
+    this.nextClick();
+    this.prevClick();
+    this.loopClick();
+  }
+
+  togglePlayPause() {
+    if (this.isPlaying) {
+      this.playBtn.classList.add("playing");
+      return;
+    }
+    this.playBtn.classList.remove("playing");
   }
 
   play() {
-    const playBtn = this.$("#play-btn");
-    playBtn.addEventListener("click", () => {
-      this.audio.play();
+    this.playBtn.addEventListener("click", () => {
+      if (this.audio.paused) {
+        this.isPlaying = true;
+        this.audio.src = this.currentSong.path;
+        this.audio.play();
+        this.audio.addEventListener("timeupdate", () => {
+          if (this.audio.currentTime === this.audio.duration) {
+            this.togglePlayPause();
+            progress.value = 0;
+            if (this.isLoop) {
+              this.audio.play();
+              this.togglePlayPause();
+              return;
+            }
+          }
+          progress.value =
+            (this.audio.currentTime / this.audio.duration) * 100 || 0;
+        });
+      } else {
+        this.isPlaying = false;
+        this.audio.pause();
+      }
+
+      this.togglePlayPause();
     });
   }
 
   progressClick() {
-    this.audio.progressClick();
+    this.progress.addEventListener("click", (e) => {
+      this.isPlaying = true;
+      const { offsetX } = e;
+      const progressWidth = progress.offsetWidth;
+      const duration = this.audio.duration;
+      this.audio.currentTime = (offsetX / progressWidth) * duration;
+    });
   }
 
-  renderTitle({ name, singer } = this.currentSong) {
-    this.$(".music-header .info .song-name").innerText = name;
-    this.$(".music-header .info .singer-name").innerText = singer || "VA";
+  loopClick() {
+    this.replayBtn.addEventListener("click", (e) => {
+      this.isLoop = !this.isLoop;
+      if (this.isLoop) {
+        this.replayBtn.classList.add("active");
+        return;
+      }
+      this.replayBtn.classList.remove("active");
+    });
   }
 
-  renderSongsList() {
-    const songsList = this.songsList.map((song, index) => {
+  nextClick() {
+    this.nextBtn.addEventListener("click", (e) => {
+      this.isPlaying = false;
+      this.togglePlayPause();
+      this.currentSongIndex =
+        this.currentSongIndex + 1 === songsList.length
+          ? 0
+          : this.currentSongIndex + 1;
+      this.currentSong = songsList[this.currentSongIndex] || songsList[0];
+      this.audio.src = this.currentSong.path;
+      this.render();
+    });
+  }
+
+  prevClick() {
+    this.prevBtn.addEventListener("click", (e) => {
+      this.isPlaying = false;
+      this.togglePlayPause();
+      this.currentSongIndex =
+        this.currentSongIndex - 1 < 0
+          ? songsList.length - 1
+          : this.currentSongIndex - 1;
+      this.currentSong = songsList[this.currentSongIndex] || songsList[0];
+      this.audio.src = this.currentSong.path;
+      this.render();
+    });
+  }
+
+  render() {
+    const songListElement = songsList.map((song, index) => {
       return `
       <li class="song">
         <div>
@@ -59,17 +145,12 @@ export default class MusicPlayer {
       </li>
       `;
     });
-    this.$(".songs-list").innerHTML = songsList.join("");
-  }
+    this.$(".music-header .info .song-name").innerText =
+      this.currentSong.name || "Unknown";
+    this.$(".music-header .info .singer-name").innerText =
+      this.currentSong.singer || "VA";
 
-  render() {
-    const songPlaying = localStorage.getItem("songPlaying") || this.currentSong;
-    this.audio.src = songPlaying.path;
-
-    this.renderTitle();
-    this.renderSongsList();
-    this.play();
-    this.progressClick();
+    this.$(".songs-list").innerHTML = songListElement.join("");
   }
 }
 
